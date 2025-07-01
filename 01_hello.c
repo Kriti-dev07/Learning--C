4ecd58a0911691df012aee950ba08afa452e35d8
Kriti-dev07 <erdeepti.ag@gmail.com>
2025-07-01 13:35:23 +0530

    Renamed Files

---
+++ 01_hello.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+printf("Hello Github!\n"); //finally made it through gitbash and gitub
+ return 0  ; 
+}
--- 02_Arithmetic.c
+++ 02_Arithmetic.c
@@ -1,5 +1,33 @@
-#include<stdio.h>
+  #include<stdio.h>
 int main(){
-printf("Hello Github!\n"); //finally made it through gitbash and gitub
- return 0  ; 
+  int number1, number2;
+  printf("enter number 1:");
+  scanf("%d", &number1);
+   
+  printf("enter number 2:");
+  scanf("%d",&number2);
+
+ 
+  //addition 
+  printf("Addition: %d +%d=%d \n", number1,number2,number1+number2);
+
+  
+  //Integer modulo
+  printf("Modulo: %d %% %d= %d\n", number1,number2,number1 % number2 );
+ 
+//float 
+  float num1, num2;
+  printf("enter number 1:");
+  scanf("%f", &num1);
+
+  printf("enter number 2:");
+  scanf("%f",&num2);
+
+
+ //multiplication 
+printf("Multiplication: %f*%f=%f \n", num1,num2,num1*num2);
+ 
+//subtraction
+printf("subtraction: %f-%f=%f \n", num1,num2,num1-num2);
+return 0  ; 
 }
